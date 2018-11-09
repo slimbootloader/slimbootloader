a74463615232fa7e5d953e2ee39df2b00ad49a40
Aiden Park <aiden.park@intel.com>
2018-11-09 09:29:29 -0800

    [APL] Rename VTd to Vtd
    Rename VTd to Vtd including directories, files and variables to follow
    EDKII coding convention.
    
    Change-Id: I0c88dcc1087eafcefb4f6d99dcd25e7b3d031a71
    Signed-off-by: Aiden Park <aiden.park@intel.com>

--- Platform/ApollolakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.h
+++ Platform/ApollolakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.h
@@ -1,6 +1,6 @@
 /** @file
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2018, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -45,7 +45,7 @@
 #include <IndustryStandard/Acpi.h>
 #include <Library/SpiFlashLib.h>
 #include <Library/TpmLib.h>
-#include <Library/VTdLib.h>
+#include <Library/VtdLib.h>
 #include <RegAccess.h>
 #include <IndustryStandard/Pci.h>
 #include <IndustryStandard/Acpi.h>
