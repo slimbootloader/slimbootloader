d313c9c5fc0125bbb5d2d7a8ea1a1d94972ad0b2
Aiden Park <aiden.park@intel.com>
2019-09-30 10:03:12 -0700

    Disable force_crlf in PatchCheck.py for now
    Slim Bootloader code has both CRLF and LF line-ending files.
    Before cleaning-up, disable force_crlf for now.
    
    Change-Id: I2e73ccfb8814ea8638c078f284ca7dbeca298e8b
    Signed-off-by: Aiden Park <aiden.park@intel.com>

--- BaseTools/Scripts/PatchCheck.py
+++ BaseTools/Scripts/PatchCheck.py
@@ -369,6 +369,8 @@ class GitDiffCheck:
 
         stripped = line.rstrip()
 
+        # TBD: Disable force_crlf in Slim Bootloader for now
+        self.force_crlf = False
         if self.force_crlf and eol != '\r\n':
             self.added_line_error('Line ending (%s) is not CRLF' % repr(eol),
                                   line)
