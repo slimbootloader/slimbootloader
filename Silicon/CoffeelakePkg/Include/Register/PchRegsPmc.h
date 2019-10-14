78db06bafb7ba6083293d01228f37a80feb4f4c8
Raghava Gudla <raghava.gudla@intel.com>
2019-10-14 14:01:32 -0700

    [WHL] Fix power button overrride smi storm issue
    power button 4 second press will cause power button override
    bit to set in PM1 status register, this bit is not cleared on
    reset and is causing SMI storm during booting to OS.
    
    Power button override bit if set is cleared now in stage1b and
    this fixed the SMI storm issue.
    
    Signed-off-by: Raghava Gudla <raghava.gudla@intel.com>

--- Silicon/CoffeelakePkg/Include/Register/PchRegsPmc.h
+++ Silicon/CoffeelakePkg/Include/Register/PchRegsPmc.h
@@ -19,6 +19,7 @@
 
 #define R_ACPI_IO_PM1_STS                        0x00
 #define B_ACPI_IO_PM1_STS_WAK                    BIT15
+#define B_ACPI_IO_PM1_STS_PRBTNOR                BIT11
 #define R_ACPI_IO_PM1_EN_MASK                    0xFFFF0000
 #define B_ACPI_IO_PM1_EN_PWRBTN_EN               BIT24
 
