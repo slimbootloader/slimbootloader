d6a5590b96515543795a77a5bb5aa12865966a7f
Maurice Ma <maurice.ma@intel.com>
2019-11-23 17:51:00 -0800

    [APL] Provide WMI service for FWU trigger
    This patch added WMI service in ACPI to provide a unified mechnism
    for user to trigger a firmware update flow. New ASL methods are
    defined. \DWMI.WQ00 is for read and \DWMI.WS00 is for write.
    
    To use it from Linux (Ubuntu):
      - Install acpi-call module
        sudo apt install acpi-call-dkms
      - Load module
        sudo modprobe acpi_call
      - Read trigger register value
        echo '\DWMI.WQ00 0' | sudo tee /proc/acpi/call
        sudo cat /proc/acpi/call
      - Write trigger register value
        echo '\DWMI.WS00 0 0x12' | sudo tee /proc/acpi/call
    
    To use it in Windows:
      - Open a command line window with admin right.
      - Run VB script to read / write the trigger register
        set Service = GetObject("winmgmts:root/wmi")
        set EnumSet = Service.InstancesOf ("AcpiFirmwareCommunication")
        for each Instance in EnumSet
          Wscript.Echo "Current  Val: " & Hex(instance.Command)
          instance.Command = 1
          instance.Put_()
          Wscript.Echo "Set New Val: " & Hex(instance.Command)
        next 'instance
    
    Signed-off-by: Maurice Ma <maurice.ma@intel.com>

---
+++ Platform/CommonBoardPkg/AcpiTables/Dsdt/AslInc.h
@@ -0,0 +1,8 @@
+/** @file
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+// Dummy include file to allow the current folder to be on the include search list.
