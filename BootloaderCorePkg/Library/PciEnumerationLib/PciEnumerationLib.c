slimbootloader/slimbootloader

commit 9fcb3a6be1ff5482c3062fc10f17a9e8879d0500

PCI resource allocation minor adjustment
This patch adjusted some alignment on PCI resource allocation so
that PCI resource is more efficently utilized. It aslo adjusted
the framebuffer MTRR range to match PCI resource allocation.

Signed-off-by: Maurice Ma <maurice.ma@intel.com>

BootloaderCorePkg/Library/PciEnumerationLib/PciEnumerationLib.c
@@ -1050,7 +1050,6 @@ CalculateResource (
     Base = ALIGN (Base, PciBarRes->PciBar->Alignment);
     PciBarRes->PciBar->BaseAddress = Base;
     Base += PciBarRes->PciBar->Length;
-
     CurrentLink = CurrentLink->ForwardLink;
   }
 
@@ -1064,7 +1063,6 @@ CalculateResource (
       Alignment = 0xFFFFF;
     }
   }
-  Base = ALIGN (Base,  Alignment);
 
   Parent->PciBar[BarType - 1].Alignment = Alignment;
   Parent->PciBar[BarType - 1].BarType   = BarType;
@@ -1307,15 +1305,13 @@ PciProgramResources (
   CurrentLink = RootBridge->ChildList.ForwardLink;
   while ((CurrentLink != NULL) && (CurrentLink != &RootBridge->ChildList)) {
     Root = PCI_IO_DEVICE_FROM_LINK (CurrentLink);
-    Root->PciBar[BarType - 1].BaseAddress = Address;
-
     CalculateResource (Root, BarType);
+    Address = ALIGN(Address, Root->PciBar[BarType - 1].Alignment);
+    Root->PciBar[BarType - 1].BaseAddress = Address;
     ProgramResource (Root, BarType);
 
     // Base Address for next device
     Address += Root->PciBar[BarType - 1].Length;
-    Address = ALIGN (Address, Root->PciBar[BarType - 1].Alignment);
-
     CurrentLink = CurrentLink->ForwardLink;
   }
 
@@ -1557,13 +1553,12 @@ PciEnumeration (
   }
 
   BaseAddress = PcdGet32 (PcdPciResourceMem32Base);
-  BaseAddress = PciProgramResources (RootBridge, PciBarTypeMem32, BaseAddress);
+  BaseAddress = PciProgramResources(RootBridge, PciBarTypeMem32, BaseAddress);
+  BaseAddress = ALIGN(BaseAddress, 0xFFFFFFF);
+  PciProgramResources(RootBridge, PciBarTypePMem32, BaseAddress);
 
-  BaseAddress = ALIGN (BaseAddress, 0xFFFFFFF);
-  PciProgramResources (RootBridge, PciBarTypePMem32, BaseAddress);
-
+  BaseAddress = PcdGet64 (PcdPciResourceMem64Base);
   if ((EnumPolicy != NULL) && (EnumPolicy->DowngradeMem64 == 0)) {
-    BaseAddress = PcdGet64 (PcdPciResourceMem64Base);
     ASSERT (BaseAddress > 0xFFFFFFFF);
     BaseAddress = PciProgramResources (RootBridge, PciBarTypeMem64, BaseAddress);
   }

Platform/ApollolakeBoardPkg/BoardConfig.py
@@ -58,8 +58,10 @@ class Board(BaseBoard):
         self.BOARD_PKG_NAME       = 'ApollolakeBoardPkg'
         self.SILICON_PKG_NAME     = 'ApollolakePkg'
 
+        self._PCI_ENUM_DOWNGRADE_PMEM64 = 1
         self.PCI_IO_BASE          = 0x00001000
         self.PCI_MEM32_BASE       = 0x80000000
+        self.PCI_MEM64_BASE       = 0x400000000
 
         self.FLASH_SIZE           = 0x800000
         self.FLASH_BASE           = self.FLASH_LAYOUT_START - self.FLASH_SIZE

Platform/ApollolakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
@@ -694,7 +694,20 @@ SetFrameBufferWriteCombining (
 {
   UINT32             MsrIdx;
   UINT32             MsrMax;
-  UINT32             Base;
+  UINT32             Data;
+  UINT32             GfxPciBase;
+  UINT64             Base64;
+
+  // Skip if GFX device does not exist
+  GfxPciBase = PCI_LIB_ADDRESS (0, 2, 0, 0);
+  if (PciRead16 (GfxPciBase) == 0xFFFF) {
+    return;
+  }
+
+  // Assume fixed 256MB prefetchable space
+  Data    = PciRead32 (GfxPciBase + PCI_BASE_ADDRESSREG_OFFSET + 12);
+  Base64  = LShiftU64 (Data, 32);
+  Base64 += (PciRead32 (GfxPciBase + PCI_BASE_ADDRESSREG_OFFSET + 8) & ~(SIZE_256MB - 1));
 
   // Enable Framebuffer as WC.
   MsrMax = EFI_MSR_CACHE_VARIABLE_MTRR_BASE +
@@ -710,8 +723,7 @@ SetFrameBufferWriteCombining (
     // Framebuffer belongs to PMEM32 in PCI resource allocation.
     // The 1st 256MB from PcdPciResourceMem32Base will be consumed by MEM32 resource.
     // And framebuffer should be allocated to the next 256MB aligned address.
-    Base = (PcdGet32 (PcdPciResourceMem32Base) + SIZE_256MB)  &  ~(SIZE_256MB - 1);
-    AsmWriteMsr64 (MsrIdx,     Base | CACHE_WRITECOMBINING);
+    AsmWriteMsr64 (MsrIdx,     Base64 | CACHE_WRITECOMBINING);
     AsmWriteMsr64 (MsrIdx + 1, 0xF00000000ULL + B_EFI_MSR_CACHE_MTRR_VALID + (UINT32)(~(SIZE_256MB - 1)));
   } else {
     DEBUG ((DEBUG_WARN, "Failed to find a free MTRR pair for framebuffer!\n"));
@@ -944,13 +956,7 @@ BoardInit (
     }
 
     SdcardPowerUp();
-
     SaveOtgRole();
-    if (PcdGetBool (PcdFramebufferInitEnabled)) {
-      // Enable framebuffer as WC for performance
-      SetFrameBufferWriteCombining ();
-    }
-
     Status = PcdSet32S (PcdFuncCpuInitHook, (UINT32)(UINTN) PlatformCpuInit);
     break;
   case PostSiliconInit:
@@ -965,7 +971,7 @@ BoardInit (
     // Initialize Smbios Info for SmbiosInit
     //
     if (FeaturePcdGet (PcdSmbiosEnabled)) {
-	  InitializeSmbiosInfo ();
+      InitializeSmbiosInfo ();
     }
     // Enable DMA protection
     if (FeaturePcdGet (PcdDmaProtectionEnabled)) {
@@ -976,6 +982,10 @@ BoardInit (
     BuildOsConfigDataHob ();
     break;
   case PostPciEnumeration:
+    if (PcdGetBool (PcdFramebufferInitEnabled)) {
+      // Enable framebuffer as WC for performance
+      SetFrameBufferWriteCombining ();
+    }
     if (PcdGetBool (PcdSeedListEnabled)) {
       Status = GenerateSeeds ();
       if (EFI_ERROR (Status)) {

commit 54bcc9b2e1f9bfcabcc2ad886e5db277754fc647

Fix P2P bridge alignement issue
The following commit 9fcb3a6be1ff5482c3062fc10f17a9e8879d0500
caused a regression on PCI bridge resource allocation. At minimum
the PCI bridge needs to have IO apperture aligned at 4KB and MMIO
apperture aligned at 1MB. The new code did not adjust the
alignment for P2P bridge following this rule.  This patch fixed
this issue.

Signed-off-by: Maurice Ma <maurice.ma@intel.com>

BootloaderCorePkg/Library/PciEnumerationLib/PciEnumerationLib.c
@@ -1057,11 +1057,12 @@ CalculateResource (
     if (Alignment < 0xFFF) {
       Alignment = 0xFFF;
     }
-    Base = ALIGN (Base,  Alignment);
+    Base = ALIGN (Base,  0xFFF);
   } else {
     if (Alignment < 0xFFFFF) {
       Alignment = 0xFFFFF;
     }
+    Base = ALIGN (Base,  0xFFFFF);
   }
 
   Parent->PciBar[BarType - 1].Alignment = Alignment;