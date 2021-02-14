6b6a0e379606e319f1ac1fb577dc1c3e06b47ea2
Guo Dong <guo.dong@intel.com>
2021-02-14 13:35:28 -0700

    [TGL] Remove WDT
    It is not necessary to set WDT for FSPM.
    So remove it to avoid potential issue.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

--- Platform/TigerlakeBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.c
+++ Platform/TigerlakeBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.c
@@ -795,12 +795,6 @@ DEBUG_CODE_END();
         break;
     }
 
-    //
-    // Set WDT timeout value and start the timer.
-    //
-    IoWrite16 (TCO_BASE_ADDRESS+TCO_TMR_REGISTER,0x258);
-    MicroSecondDelay (0x64);
-    IoAnd16 (TCO_BASE_ADDRESS + TCO1_CNT_REGISTER,(UINT16)~BIT11);
     break;
   case PostMemoryInit:
     //
@@ -822,10 +816,6 @@ DEBUG_CODE_END();
     //
     MmioAnd32 (PmcBase + R_PCH_PMC_GEN_PMCON_A, 0);
 
-    //
-    // Disable WDT
-    //
-    IoAnd16 (TCO_BASE_ADDRESS + TCO1_CNT_REGISTER,BIT11);
     break;
   case PreTempRamExit:
     break;
