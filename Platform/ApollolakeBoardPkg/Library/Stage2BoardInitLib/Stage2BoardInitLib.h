c76e3272d4ff63f34c769f7a10e28bddca9d847f
Maurice Ma <maurice.ma@intel.com>
2021-02-18 20:32:28 -0800

    Fix MTRR mask programming for GFX framebuffer
    Linux reported incorrect MTRR mask programming in SBL. This patch
    fixed this issue by using the proper MTRR mask for GFX FB.
    
    Signed-off-by: Maurice Ma <maurice.ma@intel.com>

--- Platform/ApollolakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.h
+++ Platform/ApollolakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.h
@@ -69,6 +69,7 @@
 #include <Guid/SmmInformationGuid.h>
 #include <Library/HdaLib.h>
 #include <Register/RegsSpi.h>
+#include <Register/Intel/Cpuid.h>
 #include <CseMsg.h>
 
 #define IOC_UART_PPR_CLK_N_DIV        0x64
