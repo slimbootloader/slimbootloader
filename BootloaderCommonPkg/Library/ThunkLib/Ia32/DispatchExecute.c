13733b12bfca285e47075193afc1bf206dd7330d
Maurice Ma <maurice.ma@intel.com>
2021-04-02 21:15:07 -0700

    Add thunk support from 32bit to 64bit
    This patch added thunk support from 32bit to 64bit. It allows SBL
    to call 64bit API entry from 32 bit compatible mode. It is useful
    when the payload mode is different from SBL mode.
    
    Signed-off-by: Maurice Ma <maurice.ma@intel.com>

--- BootloaderCommonPkg/Library/ThunkLib/Ia32/DispatchExecute.c
+++ BootloaderCommonPkg/Library/ThunkLib/Ia32/DispatchExecute.c
@@ -3,7 +3,7 @@
   Provide a thunk function to transition from long mode to compatibility mode to execute 32-bit code and then transit
   back to long mode.
 
-  Copyright (c) 2014 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2014 - 2021, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -13,6 +13,26 @@
 #include <Library/PagingLib.h>
 
 typedef EFI_STATUS (EFIAPI *EXECUTE_32BIT_CODE) (UINT32 Param1, UINT32 Param2);
+
+/**
+  Wrapper for a thunk to switch to long mode to execute 64-bit code and then
+  switch back to 32-bit mode.
+
+  @param[in] Function     The 64bit code entry to be executed.
+  @param[in] Param1       The first parameter to pass to 64bit code.
+  @param[in] Param2       The second parameter to pass to 64bit code.
+  @param[in] Unused       Unused.
+
+  @return Status.         Status returned from the calling function.
+**/
+EFI_STATUS
+EFIAPI
+AsmExecute64BitCode (
+  IN UINT64           Function,
+  IN UINT64           Param1,
+  IN UINT64           Param2,
+  IN UINT64           Unused
+  );
 
 /**
   Wrapper for a thunk  to transition from long mode to compatibility mode to execute 32-bit code and then transit back to
@@ -45,12 +65,12 @@ Execute32BitCode (
 
 /**
   Jump into funciton in X64 mode.
-  This function will not return.
+  This function will return back to 32bit mode.
 
-  @param[in] Function     The 32bit code entry to be executed.
-  @param[in] Param1       The first parameter to pass to 32bit code.
-  @param[in] Param2       The second parameter to pass to 32bit code.
-  @param[in] ExeInMem     If thunk needs to be executed from memory copy.
+  @param[in] Function     The 64bit code entry to be executed.
+  @param[in] Param1       The first parameter to pass to 64bit code.
+  @param[in] Param2       The second parameter to pass to 64bit code.
+  @param[in] ExeInMem     Not used.
 
   @return EFI_SUCCESS     Dummy return value.
 **/
@@ -63,14 +83,17 @@ Execute64BitCode (
   IN BOOLEAN     ExeInMem
   )
 {
-  UINTN          NewStack;
+  EFI_STATUS       Status;
 
-  NewStack = 0;
-  JumpToLongMode ((UINT64)(UINTN)Function,
-                  (UINT64)(UINTN)Param1,
-                  (UINT64)(UINTN)Param2,
-                  (UINT64)(UINTN)&NewStack);
+  LoadPageTableForLongMode ();
 
-  return EFI_SUCCESS;
+  Status = AsmExecute64BitCode (
+              (UINT64)(UINTN)Function,
+              (UINT64)(UINTN)Param1,
+              (UINT64)(UINTN)Param2,
+              (UINT64)(UINTN)0
+           );
+
+  return Status;
 }
 
