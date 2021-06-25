slimbootloader/slimbootloader

commit b78cbcf128866b8835116a6ba77d9e7ac5c8ab00

[EHL] s0ix fix for Fusa sku
Enable s0ix only for Non-Fusa sku
Removed PcdPreOsCheckerEnabled PCD flag

Signed-off-by: Ong Kok Tong <kok.tong.ong@intel.com>

Platform/ElkhartlakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
@@ -216,6 +216,8 @@ SI_PCH_DEVICE_INTERRUPT_CONFIG mPchDevIntConfig[] = {
   {16, 7, SiPchIntB, PchPIRQB}  // THC #1
 };
 
+STATIC UINT8 mPchSciSupported = 0xFF;
+
 UINT8
 GetSerialPortStrideSize (
   VOID
@@ -662,11 +664,15 @@ BoardInit (
   UINT32                      TsegBase;
   UINT32                      TsegSize;
 
+  if (mPchSciSupported == 0xFF){
+    mPchSciSupported = PchIsSciSupported();
+  }
+
   switch (InitPhase) {
   case PreSiliconInit:
     EnableLegacyRegions ();
     ConfigureGpio (CDATA_GPIO_TAG, 0, NULL);
-    if (FeaturePcdGet (PcdPreOsCheckerEnabled) && PchIsSciSupported ()) {
+    if (mPchSciSupported) {
       DEBUG ((DEBUG_INFO, "GpioPadConfigTable for Fusa\n"));
       ConfigureGpio (CDATA_NO_TAG, ARRAY_SIZE(mGpioTablePreMemEhlFusa), (UINT8*)mGpioTablePreMemEhlFusa);
     }
@@ -707,7 +713,7 @@ BoardInit (
   case PostPciEnumeration:
     // Set pre-OS checker features flag
     LdrGlobal = (LOADER_GLOBAL_DATA *)GetLoaderGlobalDataPointer ();
-    if (FeaturePcdGet (PcdPreOsCheckerEnabled) && PchIsSciSupported ()) {
+    if (mPchSciSupported) {
       if (!SciBootSuccess ()) {
         DEBUG ((DEBUG_WARN, "SCI device has boot issue\n"));
       }
@@ -1130,8 +1136,7 @@ UpdateFspConfig (
     Fspscfg->XdciEnable          = SiCfgData->XdciEnable;
     FeaturesCfgData               = (FEATURES_CFG_DATA *) FindConfigDataByTag(CDATA_FEATURES_TAG);
     if (FeaturesCfgData != NULL) {
-      if (FeaturesCfgData->Features.LowPowerIdle != 0){
-        DEBUG ((DEBUG_INFO, "FeaturesCfgData->Features.LowPowerIdle = 0x%x\n",FeaturesCfgData->Features.LowPowerIdle));
+      if (FeaturesCfgData->Features.LowPowerIdle != 0 && mPchSciSupported != 1){
         Fspscfg->XdciEnable          = 0;
       }
     }
@@ -1542,7 +1547,7 @@ UpdateFspConfig (
     ASSERT (SiCfgData != NULL);
   } //end of SiCfgData
 
-  if (FeaturePcdGet (PcdPreOsCheckerEnabled) && PchIsSciSupported ()) {
+  if (mPchSciSupported) {
     Fspscfg->IsFusaSupported = 0x1;
     Fspscfg->IehMode = 0x1;
     //
@@ -1678,7 +1683,7 @@ UpdateFspConfig (
     Fspscfg->CstateLatencyControl5Irtl     = PowerCfgData->CstateLatencyControl5Irtl;
   }
 
-  if (FeaturePcdGet (PcdPreOsCheckerEnabled) && PchIsSciSupported ()) {
+  if (mPchSciSupported) {
     DEBUG ((DEBUG_INFO, "Applying Fusa FSP UPD settings.........\n"));
     Fspscfg->Eist                          = 0;            // Intel Speed Step->EnableGv
     Fspscfg->Hwp                           = 0;            // Intel Speed Shift
@@ -1841,7 +1846,7 @@ UpdateOsBootMediumInfo (
 
   FillBootOptionListFromCfgData (OsBootOptionList);
   // Disable PreOS checker since the SKU doesn't support it
-  if (!PchIsSciSupported ()) {
+  if (mPchSciSupported) {
     for (Idx = 0; Idx < OsBootOptionList->OsBootOptionCount; Idx++) {
       BootOption = &(OsBootOptionList->OsBootOption[Idx]);
       if ((BootOption->BootFlags & BOOT_FLAGS_PREOS) != 0) {

BootloaderCommonPkg/BootloaderCommonPkg.dec
@@ -272,8 +272,6 @@
   gPlatformCommonLibTokenSpaceGuid.PcdSourceDebugEnabled      | FALSE      | BOOLEAN | 0x20000212
   gPlatformCommonLibTokenSpaceGuid.PcdContainerBootEnabled    | FALSE      | BOOLEAN | 0x20000213
   gPlatformCommonLibTokenSpaceGuid.PcdEmmcHs400SupportEnabled | TRUE       | BOOLEAN | 0x20000214
-  # Determine if the Pre-OS checker should be executed or not.
-  gPlatformCommonLibTokenSpaceGuid.PcdPreOsCheckerEnabled     | FALSE      | BOOLEAN | 0x20000215
   # This PCD will force to initialize SerialPort regardless of its initialized state
   gPlatformCommonLibTokenSpaceGuid.PcdForceToInitSerialPort   | FALSE      | BOOLEAN | 0x20000216
   # This PCD will enable very basic debug Shell so that it can work in early stage such as 1A/1B/2.

BootloaderCorePkg/BootloaderCorePkg.dsc
@@ -319,7 +319,6 @@
   gPayloadTokenSpaceGuid.PcdCsmeUpdateEnabled             | $(ENABLE_CSME_UPDATE)
   gPlatformModuleTokenSpaceGuid.PcdLegacyEfSegmentEnabled | $(ENABLE_LEGACY_EF_SEG)
   gPlatformCommonLibTokenSpaceGuid.PcdEmmcHs400SupportEnabled | $(ENABLE_EMMC_HS400)
-  gPlatformCommonLibTokenSpaceGuid.PcdPreOsCheckerEnabled | $(ENABLE_PRE_OS_CHECKER)
   gPlatformCommonLibTokenSpaceGuid.PcdDmaProtectionEnabled | $(ENABLE_DMA_PROTECTION)
   gPlatformCommonLibTokenSpaceGuid.PcdMultiUsbBootDeviceEnabled |  $(ENABLE_MULTI_USB_BOOT_DEV)
   gPlatformCommonLibTokenSpaceGuid.PcdCpuX2ApicEnabled    | $(SUPPORT_X2APIC)

PayloadPkg/OsLoader/OsLoader.inf
@@ -109,7 +109,6 @@
   gPlatformCommonLibTokenSpaceGuid.PcdFrameBufferMaxConsoleHeight
   gPayloadTokenSpaceGuid.PcdGrubBootCfgEnabled
   gPlatformCommonLibTokenSpaceGuid.PcdContainerBootEnabled
-  gPlatformCommonLibTokenSpaceGuid.PcdPreOsCheckerEnabled
   gPlatformCommonLibTokenSpaceGuid.PcdMeasuredBootHashMask
 
 [Depex]

Platform/ElkhartlakeBoardPkg/AcpiTables/Fadt/Fadt6.1.h
@@ -67,8 +67,7 @@
   EFI_ACPI_6_1_RTC_S4 | \
   EFI_ACPI_6_1_SLP_BUTTON | \
   EFI_ACPI_6_1_PROC_C1 | \
-  EFI_ACPI_6_1_RESET_REG_SUP | \
-  EFI_ACPI_6_1_LOW_POWER_S0_IDLE_CAPABLE  \
+  EFI_ACPI_6_1_RESET_REG_SUP \
   )
 
 

Platform/ElkhartlakeBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.c
@@ -236,10 +236,12 @@ UpdateFspConfig (
   UINT32                        Index;
   UINT8                         DebugPort;
   FEATURES_CFG_DATA             *FeaturesCfgData;
+  BOOLEAN                       PchSciSupported;
 
   FspmUpd                       = (FSPM_UPD *)FspmUpdPtr;
   FspmArchUpd                   = &FspmUpd->FspmArchUpd;
   Fspmcfg                       = &FspmUpd->FspmConfig;
+  PchSciSupported               = PchIsSciSupported ();
 
   DebugPort = GetDebugPort ();
   if (DebugPort < PCH_MAX_SERIALIO_UART_CONTROLLERS) {
@@ -314,8 +316,7 @@ UpdateFspConfig (
     Fspmcfg->TcssXhciEn                 = MemCfgData->TcssXhciEn;
     Fspmcfg->TcssXdciEn                 = MemCfgData->TcssXdciEn;
     if (FeaturesCfgData != NULL) {
-      if (FeaturesCfgData->Features.LowPowerIdle != 0){
-        DEBUG ((DEBUG_INFO, "FeaturesCfgData->Features.LowPowerIdle = 0x%x\n",FeaturesCfgData->Features.LowPowerIdle));
+      if (FeaturesCfgData->Features.LowPowerIdle != 0 && PchSciSupported != 1){
         Fspmcfg->TcssXdciEn             = 0;
       }
     }
@@ -620,11 +621,11 @@ UpdateFspConfig (
         for (Index = 0; Index < PCH_MAX_HDA_SSP_LINK_NUM; Index++) {
           Fspmcfg->PchHdaAudioLinkSspEnable[Index]      = 0;
         }
-        if (!FeaturePcdGet (PcdPreOsCheckerEnabled) && !PchIsSciSupported ()) {
+        if (PchSciSupported != 1) {
           Fspmcfg->PchHdaAudioLinkSspEnable[0]          = 1;
         }
         for (Index = 0; Index < PCH_MAX_HDA_DMIC_LINK_NUM; Index++) {
-          if (FeaturePcdGet (PcdPreOsCheckerEnabled) && PchIsSciSupported ()) {
+          if (PchSciSupported) {
             Fspmcfg->PchHdaAudioLinkDmicEnable[Index]   = 0;
           }
         }

Platform/ElkhartlakeBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.inf
@@ -57,4 +57,3 @@
   gPlatformCommonLibTokenSpaceGuid.PcdMeasuredBootEnabled
   gPlatformModuleTokenSpaceGuid.PcdAcpiEnabled
   gPlatformModuleTokenSpaceGuid.PcdVtdEnabled
-  gPlatformCommonLibTokenSpaceGuid.PcdPreOsCheckerEnabled

Platform/ElkhartlakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.inf
@@ -92,7 +92,6 @@
   gPlatformModuleTokenSpaceGuid.PcdSmmRebaseMode
   gPlatformModuleTokenSpaceGuid.PcdSmramTsegBase
   gPlatformModuleTokenSpaceGuid.PcdSmramTsegSize
-  gPlatformCommonLibTokenSpaceGuid.PcdPreOsCheckerEnabled
   gPlatformCommonLibTokenSpaceGuid.PcdMeasuredBootEnabled
   gPlatformModuleTokenSpaceGuid.PcdPsdBiosEnabled
   gPlatformModuleTokenSpaceGuid.PcdSmbiosStringsPtr

commit 279bf877e827c2b243c1d85ce08fe70d6f195d7b

[EHL] Fix Stage2 Boot Option checking
Fix mPchSciSupported flag checking before changing the
flag value of boot option.
This bug was introduced from commit
b78cbcf128866b8835116a6ba77d9e7ac5c8ab00

Signed-off-by: Ong Kok Tong <kok.tong.ong@intel.com>

Platform/ElkhartlakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
@@ -1846,7 +1846,7 @@ UpdateOsBootMediumInfo (
 
   FillBootOptionListFromCfgData (OsBootOptionList);
   // Disable PreOS checker since the SKU doesn't support it
-  if (mPchSciSupported) {
+  if (!mPchSciSupported) {
     for (Idx = 0; Idx < OsBootOptionList->OsBootOptionCount; Idx++) {
       BootOption = &(OsBootOptionList->OsBootOption[Idx]);
       if ((BootOption->BootFlags & BOOT_FLAGS_PREOS) != 0) {