de76d5d9338eda9af8a36f06d43febc77817b574
Maurice Ma <maurice.ma@intel.com>
2021-10-02 19:25:55 -0700

    Add accurate TSC frequency calculation support
    Current SBL code uses MSR(0xce) to calculate the CPU TSC frequency.
    However, it is not very accurate. A better way is to use CPUID to
    calculate the TSC frequency. This patch added new API to get accurate
    TSC frequency.  It also added APIs to allow conversion between time
    and TSC ticks.
    
    Signed-off-by: Maurice Ma <maurice.ma@intel.com>

--- BootloaderCommonPkg/Include/Library/TimeStampLib.h
+++ BootloaderCommonPkg/Include/Library/TimeStampLib.h
@@ -1,6 +1,6 @@
 /** @file
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2021, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -30,6 +30,49 @@ UINT32
 EFIAPI
 GetTimeStampFrequency (
   VOID
+  );
+
+/**
+  Get timestamp accurate frequency in HZ by CPUID.
+
+  The TSC counting frequency is determined by using CPUID leaf 0x15. Frequency in MHz = Core XTAL frequency * EBX/EAX.
+  In newer flavors of the CPU, core xtal frequency is returned in ECX or 0 if not supported.
+
+  @retval The number of TSC counts per second.
+
+**/
+UINT64
+EFIAPI
+GetTimeStampAccurateFrequency (
+  VOID
+  );
+
+/**
+  Convert timestamp ticks to microseconds.
+
+  @param[in]  Ticks  The number of timestamp ticks to convert.
+
+  @retval     MicroSeconds
+
+**/
+UINT64
+EFIAPI
+TimeStampTickToMicroSecond (
+  IN UINT64    Ticks
+  );
+
+/**
+  Convert microseconds to timestamp ticks.
+
+  @param[in]  MicroSeconds  The number of microseconds to convert.
+
+  @retval     Timestamp ticks
+
+**/
+UINT64
+EFIAPI
+MicroSecondToTimeStampTick (
+  IN UINT64    MicroSeconds
   );
 
 #endif
