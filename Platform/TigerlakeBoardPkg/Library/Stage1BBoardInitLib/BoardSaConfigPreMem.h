58e865cef6bcd0dda904bee5d847f4bba8489489
Jim <jim.pelner@intel.com>
2021-10-20 08:48:44 -0700

    [TGLU] Expand LPDDR4 DDI Config Array to 16 Entries
    The DDI config array for LPDDR4 was incorrectly defined as 13 entries.
    There are 16 UPDs that are programmed in UpdateFspConfig, resulting in
    random UPD assignments.
    
    This addresses issue
    https://github.com/slimbootloader/slimbootloader/issues/1365#issue-1031580997
    
    Signed-off-by: Jim <jim.pelner@intel.com>

--- Platform/TigerlakeBoardPkg/Library/Stage1BBoardInitLib/BoardSaConfigPreMem.h
+++ Platform/TigerlakeBoardPkg/Library/Stage1BBoardInitLib/BoardSaConfigPreMem.h
@@ -53,15 +53,18 @@ GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 mTglUDdr4RowDisplayDdiConfig[16] = {
 //
 // Display DDI settings for TGL-U LP DDR4
 //
-GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 mTglULpDdr4RowDisplayDdiConfig[13] = {
-                                                                                DdiPortEdp,     // DDI Port A Config : DdiPortADisabled = Disabled, DdiPortAEdp = eDP, DdiPortAMipiDsi = MIPI DSI
-                                                                                DdiHpdDisable,   // DDI Port B HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 mTglULpDdr4RowDisplayDdiConfig[16] = {
+                                                                                DdiPortEdp,      // DDI Port A Config : DdiPortDisabled = No LFP is Connected, DdiPortEdp = eDP, DdiPortMipiDsi = MIPI DSI
+                                                                                DdiPortDisabled, // DDI Port B Config : DdiPortDisabled = No LFP is Connected, DdiPortEdp = eDP, DdiPortMipiDsi = MIPI DSI
+                                                                                DdiHpdDisable,   // DDI Port A HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                                DdiHpdEnable,    // DDI Port B HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                                 DdiHpdDisable,   // DDI Port C HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                                 DdiHpdDisable,   // DDI Port 1 HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                                 DdiHpdDisable,   // DDI Port 2 HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                                 DdiHpdDisable,   // DDI Port 3 HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                                 DdiHpdDisable,   // DDI Port 4 HPD : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                                DdiDisable,      // DDI Port B DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                                DdiDisable,      // DDI Port A DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                                DdiDdcEnable,    // DDI Port B DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
                                                                                 DdiDisable,      // DDI Port C DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
                                                                                 DdiDisable,      // DDI Port 1 DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
                                                                                 DdiDisable,      // DDI Port 2 DDC : DdiDisable = Disable, DdiDdcEnable = Enable DDC
