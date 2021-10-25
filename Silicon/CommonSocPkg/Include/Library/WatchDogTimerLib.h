80ce17607c5eafaff5c69004d3fe36c8a7dac8b7
Guo Dong <guo.dong@intel.com>
2021-10-25 16:54:57 -0700

    Add a common watch dog timer
    This library provides a watch dog timer instance using
    Over-Clocking Watchdog Timer.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

---
+++ Silicon/CommonSocPkg/Include/Library/WatchDogTimerLib.h
@@ -0,0 +1,110 @@
+/** @file
+  Header file for watch dog timer library implementation.
+
+  Copyright (c) 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#ifndef WATCH_DOG_TIMBER_LIB_H_
+#define WATCH_DOG_TIMBER_LIB_H_
+
+#define  WDT_TIMEOUT_TCC_DSO   200          // 200 seconds
+#define  WDT_FLAG_TCC_DSO      BIT17
+#define  WDT_FLAG_MASK         BIT17
+
+
+/**
+  Reloads WDT with new timeout value and starts it.
+
+  @param[in] TimeoutValue         Time in seconds before WDT times out. Supported range = 1 - 1024.
+  @param[in] Flags                WDT timer flag that indicate the purpose to start the WDT timer.
+
+  @retval EFI_SUCCESS             if everything's OK
+  @retval EFI_INVALID_PARAMETER   if TimeoutValue parameter is wrong
+**/
+EFI_STATUS
+EFIAPI
+WdtReloadAndStart (
+  IN  UINT32  TimeoutValue,
+  IN  UINT32  Flags
+  );
+
+
+/**
+  Disables WDT timer and cleared the specified WDT timer flag.
+
+  @param[in] Flags                The timer flags will be cleared when disabling the WDT
+
+**/
+VOID
+EFIAPI
+WdtDisable (
+  IN  UINT32  Flags
+  );
+
+
+/**
+  Clear WDT timer flags.
+
+  @param[in] Flags             The timer flags that would be cleared.
+
+**/
+VOID
+EFIAPI
+WdtClearFlags (
+  IN  UINT32  Flags
+  );
+
+/**
+  Returns if the previous reset is triggered by timer expiration.
+
+  @retval TRUE                    Timer time out
+  @retval FALSE                   Timer no time out
+**/
+UINT8
+EFIAPI
+IsWdtTimeout (
+  VOID
+  );
+
+/**
+  Returns if specified timer flag is set.
+
+  @param[in] Flags                The timer flags that would be checked.
+
+  @retval TRUE                    if specified timer flags is set
+  @retval FALSE                   if specified timer flags is not set
+**/
+UINT8
+EFIAPI
+IsWdtFlagsSet (
+  IN  UINT32  Flags
+  );
+
+
+/**
+  Returns WDT enabled/disabled status.
+
+  @retval TRUE                    if WDT is enabled
+  @retval FALSE                   if WDT is disabled
+**/
+UINT8
+EFIAPI
+IsWdtEnabled (
+  VOID
+  );
+
+
+/**
+  Returns WDT locked status.
+
+  @retval TRUE                    if WDT is locked
+  @retval FALSE                   if WDT is unlocked
+**/
+UINT8
+EFIAPI
+IsWdtLocked (
+  VOID
+  );
+
+#endif
