7400d6f0b3c1ce1d710b2b6097944ce67229faf0
Vincent Chen <vincent.chen@intel.com>
2022-04-23 01:17:14 +0800

    fix the wrong sblopen_dir value in StitchLoader.py and StitchIfwi.py
    This patch can reduce the chance of setting SBL_SOURCE when the
    SBL root directory is not named as "SblOpen" or when the scripts
    are run from the path other than the SBL root directory. Also note
    that os.path.abspath() returns the absolute path relative to the
    current working directory instead of the real path of __file__.
    So in StitchIfwi.py, sblopen_dir was incorrect since the working
    directory had been changed before calling stitch().
    
    Signed-off-by: Vincent Chen <vincent.chen@intel.com>

--- Platform/TigerlakeBoardPkg/Script/StitchLoader.py
+++ Platform/TigerlakeBoardPkg/Script/StitchLoader.py
@@ -1,7 +1,7 @@
 ## @ StitchLoader.py
-#  This is a python stitching script for Slim Bootloader WHL/CFL build
+#  This is a python stitching script for Slim Bootloader TGL build
 #
-# Copyright (c) 2019, Intel Corporation. All rights reserved. <BR>
+# Copyright (c) 2019 - 2022, Intel Corporation. All rights reserved. <BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #
 ##
@@ -12,7 +12,7 @@ from   ctypes import *
 from   functools import reduce
 
 sys.dont_write_bytecode = True
-sblopen_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../../../', 'SblOpen')
+sblopen_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
 if not os.path.exists (sblopen_dir):
     sblopen_dir = os.getenv('SBL_SOURCE', '')
 sys.path.append (os.path.join(sblopen_dir, "BootloaderCorePkg" , "Tools"))
