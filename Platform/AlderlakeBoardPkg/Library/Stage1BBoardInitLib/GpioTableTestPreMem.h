50e9dda4c7160217e056285a09015af079e80a0f
Guo Dong <guo.dong@intel.com>
2022-05-01 13:50:59 -0700

    [TEST-S] Update GPIO for test boards
    Use the GPIO table for the test boards.
    This change would help fix PCIe device detection from PCIe slot.
    Late would change post memory gpio table to SBL cfg data.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

--- Platform/AlderlakeBoardPkg/Library/Stage1BBoardInitLib/GpioTableTestPreMem.h
+++ Platform/AlderlakeBoardPkg/Library/Stage1BBoardInitLib/GpioTableTestPreMem.h
@@ -28,19 +28,45 @@ GLOBAL_REMOVE_IF_UNREFERENCED GPIO_INIT_CONFIG mGpioTableEarlyPreMemTestSDdr5UDi
   {GPIO_VER4_S_GPP_F13,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 3 - X2 CONNECTOR Reset
 
   {GPIO_VER4_S_GPP_K11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 1 Power enable
-//{GPIO_VER4_S_GPP_C10,  {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 1 Reset
 
   {GPIO_VER4_S_GPP_B22,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 2 Power enable
-//{GPIO_VER4_S_GPP_F16,  {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 2 Reset
 
   {GPIO_VER4_S_GPP_K2,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 3 Power enable
-//{GPIO_VER4_S_GPP_B6,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 3 Reset
 
   {GPIO_VER4_S_GPP_G1,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioHostDeepReset,  GpioTermNone }}, //Thunderbolt CIO_PWR_EN (CIO_PWR_EN needs set to high before PCIe SLOT_2 PERST set to high; at least 10ms)
   {GPIO_VER4_S_GPP_F5,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioHostDeepReset,  GpioTermNone }}, //TBT_RTD3_PWR_EN
 
   {GPIO_VER4_S_GPP_H0,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirIn,      GpioOutDefault,  GpioIntDefault,          GpioResetDefault,   GpioTermNone }}, //VAL_SV_ADVANCE_STRAP/DISPLAY_ID_1
   {GPIO_VER4_S_GPP_J8,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirIn ,     GpioOutDefault,  GpioIntDefault,          GpioResetDefault,   GpioTermNone }}, //VAL_TEST_SETUP_MENU/DISPLAY_ID_0
+  {GPIO_VER4_S_GPP_E7,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},  // GPIO_TBT_RST_N
+};
+
+GLOBAL_REMOVE_IF_UNREFERENCED GPIO_INIT_CONFIG mGpioTableEarlyPreMemTestSDdr5SODimm1DRvp[] =
+{
+  {GPIO_VER4_S_GPP_F4,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU PEG Slot1 Power enable
+  {GPIO_VER4_S_GPP_E2,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU PEG Slot1 Reset
+
+  {GPIO_VER4_S_GPP_H16,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU M.2 SSD Slot Power enable
+
+  {GPIO_VER4_S_GPP_E1,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 1 - X4 CONNECTOR Power enable
+  {GPIO_VER4_S_GPP_F11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 1 - X4 CONNECTOR Reset
+
+  {GPIO_VER4_S_GPP_H11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 2 - X4 CONNECTOR Power enable
+  {GPIO_VER4_S_GPP_F12,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 2 - X4 CONNECTOR Reset
+
+  {GPIO_VER4_S_GPP_B21,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 3 - X2 CONNECTOR Power enable
+  {GPIO_VER4_S_GPP_F13,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 3 - X2 CONNECTOR Reset
+
+  {GPIO_VER4_S_GPP_K11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 1 Power enable
+  //{GPIO_VER4_S_GPP_C10,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 1 Reset
+
+  {GPIO_VER4_S_GPP_B22,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 2 Power enable,not used on S08
+  //{GPIO_VER4_S_GPP_F16,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 2 Reset
+
+  {GPIO_VER4_S_GPP_K2,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 3 Power enable
+  //{GPIO_VER4_S_GPP_B6,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 3 Reset
+  {GPIO_VER4_S_GPP_F5,    {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioResetDefault,   GpioTermNone}}, //POR Thunderbolt RTD3 enable,and CIO_PWR_EN
+  {0x0}
 };
 
 #endif
