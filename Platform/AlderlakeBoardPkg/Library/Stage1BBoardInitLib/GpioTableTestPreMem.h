f431adc4ba702019e871a609fed13c6fe86abda1
Guo Dong <guo.dong@intel.com>
2022-07-22 22:48:52 -0700

    [TESTS] revert the premem gpio change for S17 board
    Premem GPIO table for S17 board caused boot hang,
    so revert the change back.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

--- Platform/AlderlakeBoardPkg/Library/Stage1BBoardInitLib/GpioTableTestPreMem.h
+++ Platform/AlderlakeBoardPkg/Library/Stage1BBoardInitLib/GpioTableTestPreMem.h
@@ -41,32 +41,4 @@ GLOBAL_REMOVE_IF_UNREFERENCED GPIO_INIT_CONFIG mGpioTableEarlyPreMemTestSDdr5UDi
   {GPIO_VER4_S_GPP_E7,   {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},  // GPIO_TBT_RST_N
 };
 
-GLOBAL_REMOVE_IF_UNREFERENCED GPIO_INIT_CONFIG mGpioTableEarlyPreMemTestSDdr5SODimm1DRvp[] =
-{
-  {GPIO_VER4_S_GPP_F4,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU PEG Slot1 Power enable
-  {GPIO_VER4_S_GPP_E2,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU PEG Slot1 Reset
-
-  {GPIO_VER4_S_GPP_H16,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //CPU M.2 SSD Slot Power enable
-
-  {GPIO_VER4_S_GPP_E1,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 1 - X4 CONNECTOR Power enable
-  {GPIO_VER4_S_GPP_F11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 1 - X4 CONNECTOR Reset
-
-  {GPIO_VER4_S_GPP_H11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 2 - X4 CONNECTOR Power enable
-  {GPIO_VER4_S_GPP_F12,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 2 - X4 CONNECTOR Reset
-
-  {GPIO_VER4_S_GPP_B21,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 3 - X2 CONNECTOR Power enable
-  {GPIO_VER4_S_GPP_F13,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCIE SLOT 3 - X2 CONNECTOR Reset
-
-  {GPIO_VER4_S_GPP_K11,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 1 Power enable
-  //{GPIO_VER4_S_GPP_C10,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 1 Reset
-
-  {GPIO_VER4_S_GPP_B22,  {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 2 Power enable,not used on S08
-  //{GPIO_VER4_S_GPP_F16,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 2 Reset
-
-  {GPIO_VER4_S_GPP_K2,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioPlatformReset,  GpioTermNone }}, //PCH M.2 SSD Slot 3 Power enable
-  //{GPIO_VER4_S_GPP_B6,   {GpioPadModeGpio, GpioHostOwnAcpi, GpioDirOut,     GpioOutLow,      GpioIntDefault,          GpioPlatformReset,  GpioTermNone}},//PCH M.2 SSD Slot 3 Reset
-  {GPIO_VER4_S_GPP_F5,    {GpioPadModeGpio, GpioHostOwnGpio, GpioDirOut,     GpioOutHigh,     GpioIntDefault,          GpioResetDefault,   GpioTermNone}}, //POR Thunderbolt RTD3 enable,and CIO_PWR_EN
-  {0x0}
-};
-
 #endif
