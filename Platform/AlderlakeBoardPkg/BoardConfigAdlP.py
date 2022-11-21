2e413a6843e43992f1f0f4c83df8f0e318adf7f1
Vincent Chen <vincent.chen@intel.com>
2022-11-22 01:29:03 +0800

    [ADLP] Update FSP/UCODE/platform version for MR2 release
    - update FSP version to IoT ADL-P MR2 (0C.01.73.10)
    - update Microcode version to 423
    - update platform version to 1.2
    
    Signed-off-by: Vincent Chen <vincent.chen@intel.com>

--- Platform/AlderlakeBoardPkg/BoardConfigAdlP.py
+++ Platform/AlderlakeBoardPkg/BoardConfigAdlP.py
@@ -23,7 +23,7 @@ class Board(AlderlakeBoardConfig.Board):
         self.VERINFO_IMAGE_ID     = 'SB_ADLP'
         self.BOARD_NAME           = 'adlp'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 1
+        self.VERINFO_PROJ_MINOR_VER = 2
         self._EXTRA_INC_PATH      = ['Silicon/AlderlakePkg/Adlp/Include']
         self._FSP_PATH_NAME       = 'Silicon/AlderlakePkg/Adlp/FspBin'
         self.MICROCODE_INF_FILE   = 'Silicon/AlderlakePkg/Microcode/Microcode.inf'
