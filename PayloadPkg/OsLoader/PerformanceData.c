e092f623405c878d56bfd6ec1f21d1bffff1f98b
Atharva Lele <atharva.lele@intel.com>
2022-12-01 12:15:54 -0800

    OsLoader: remove references to IAS images
    Change IAS references to 'Boot Image' since IAS support is deprecated.
    
    Signed-off-by: Atharva Lele <atharva.lele@intel.com>

--- PayloadPkg/OsLoader/PerformanceData.c
+++ PayloadPkg/OsLoader/PerformanceData.c
@@ -39,9 +39,9 @@ LinuxPerfIdToStr (
   case 0x4070:
     return "Load boot images";
   case 0x4080:
-    return "Verify IAS image";
+    return "Verify boot image";
   case 0x40A0:
-    return "Process IAS type";
+    return "Process boot image type";
   case 0x40B0:
     return "Process ELF/MultiBoot";
   case 0x40E0:
