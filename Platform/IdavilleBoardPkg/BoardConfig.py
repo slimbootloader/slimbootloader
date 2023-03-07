f0b13a0941604977a953f3e30f00e32efe880c5c
cshur <cs.hur@intel.com>
2023-03-07 11:30:47 -0700

    [ICXD][LCC] Update Microcode for both LCC and HCC (#1839)
    Version update 1.1 to 1.2.
    Update Microcode 201 to 230
    
    Signed-off-by: cshur <cs.hur@intel.com>

--- Platform/IdavilleBoardPkg/BoardConfig.py
+++ Platform/IdavilleBoardPkg/BoardConfig.py
@@ -1,7 +1,7 @@
 ## @file
 # This file is used to provide board specific image information.
 #
-#  Copyright (c) 2020 - 20222 Intel Corporation. All rights reserved.<BR>
+#  Copyright (c) 2020 - 2023 Intel Corporation. All rights reserved.<BR>
 #
 #  SPDX-License-Identifier: BSD-2-Clause-Patent
 #
@@ -24,7 +24,7 @@ class Board(BaseBoard):
 
         self.VERINFO_IMAGE_ID     = 'SB_IDV\0'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 0
+        self.VERINFO_PROJ_MINOR_VER = 2
         self.VERINFO_SVN          = 1
         self.VERINFO_BUILD_DATE   = time.strftime("%m/%d/%Y")
 
