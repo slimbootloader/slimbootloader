bfb7ec38bc30c0a9b0a4273bb3c7e6aee8508add
Sean McGinn <sean.mcginn@intel.com>
2023-03-20 15:36:22 -0700

    Enhance TPM2 Logs
    Show error message whenever an unsupported PCR bank is used
    
    Signed-off-by: Sean McGinn <sean.mcginn@intel.com>

--- BootloaderCommonPkg/Library/TpmLib/Tpm2Capability.c
+++ BootloaderCommonPkg/Library/TpmLib/Tpm2Capability.c
@@ -254,6 +254,11 @@ Tpm2GetCapabilitySupportedAndActivePcrs (
           *ActivePcrBanks |= HASH_ALG_SM3_256;
         }
         break;
+
+      default:
+        DEBUG ((DEBUG_VERBOSE, "GetSupportedAndActivePcrs - Unsupported bank 0x%04x.\n", Pcrs.pcrSelections[Index].hash));
+        continue;
+        break;
       }
     }
   }
