1ea4a0a4da74080b0dcf987d8838deeda3da8874
Randy <randy.lin@intel.com>
2023-06-30 01:19:27 +0800

    fix: [Common] Unchecked return value for HeciGetMeMode
    If the function returns an error value, the error value
    may be mistaken for a normal value.
    In HeciSend: Value returned from a function is
    not checked for errors before being used. (CWE-252)
    
    Signed-off-by: Randy <randy.lin@intel.com>

--- Silicon/CommonSocPkg/Library/HeciLib/HeciCore.c
+++ Silicon/CommonSocPkg/Library/HeciLib/HeciCore.c
@@ -741,7 +741,13 @@ HeciSend (
     return EFI_DEVICE_ERROR;
   }
 
-  HeciGetMeMode (&MeMode);
+  Status = HeciGetMeMode (&MeMode);
+  if (!EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_INFO, "ME Mode: %X\n", MeMode));
+  } else {
+    DEBUG ((DEBUG_INFO, "Failed to Get ME Mode!\n"));
+    return EFI_UNSUPPORTED;
+  }
   if (MeMode == ME_MODE_SECOVER) {
     return EFI_UNSUPPORTED;
   }
