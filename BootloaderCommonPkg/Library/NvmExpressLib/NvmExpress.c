slimbootloader/slimbootloader

commit 0d45c3e60a93f3b43a252f8f4600a7e931ecf331

fix: [Common] Fix Coverity issues (#1958)
Fixed dead default in switch and logically dead code (CWE 561)

Signed-off-by: Kevin Tsai <kevin.tsai@intel.com>

BootloaderCommonPkg/Library/NvmExpressLib/NvmExpress.c
@@ -2,7 +2,7 @@
   NvmExpress driver is used to manage non-volatile memory subsystem which follows
   NVM Express specification.
 
-  Copyright (c) 2013 - 2021, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2013 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -177,7 +177,7 @@ Exit:
     FreePool (NamespaceData);
   }
 
-  if (EFI_ERROR (Status) && (Device != NULL)) {
+  if (Device != NULL) {
     FreePool (Device);
   }
   return Status;

BootloaderCommonPkg/Library/AhciLib/AhciDevice.c
@@ -1,7 +1,7 @@
 /** @file
   This file provides AHCI SATA device block access interfaces.
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -163,10 +163,6 @@ AhciReadWriteBlock (
 
   ReadBuf               = Buffer;
   LbaIndex              = Lba;
-
-  if (AtaDevice == NULL) {
-    return EFI_INVALID_PARAMETER;
-  }
 
   if (Buffer == NULL) {
     return EFI_INVALID_PARAMETER;

BootloaderCommonPkg/Library/LoaderPerformanceLib/LoaderPerformancePrintLib.c
@@ -1,6 +1,6 @@
 /** @file
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -481,9 +481,6 @@ PrintCsmePerfData (
   }
 
   CsmePerformanceInfo = (CSME_PERFORMANCE_INFO *)GET_GUID_HOB_DATA (GuidHob);
-  if (CsmePerformanceInfo == NULL) {
-    return;
-  }
 
   DEBUG ((DEBUG_INFO | DEBUG_EVENT, "\n Csme Boot Time Performance information\n"));
 

BootloaderCommonPkg/Library/MmcAccessLib/SdMmcPciHci.c
@@ -4,7 +4,7 @@
 
   It would expose EFI_SD_MMC_PASS_THRU_PROTOCOL for upper layer use.
 
-  Copyright (c) 2015 - 2020, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2015 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -140,9 +140,6 @@ SdMmcHcRwMmio (
       MmioWrite64 (Address + Offset, * (UINT64 *)Data);
     }
     break;
-  default:
-    ASSERT (FALSE);
-    return EFI_INVALID_PARAMETER;
   }
 
   return EFI_SUCCESS;

BootloaderCommonPkg/Library/NvmExpressLib/NvmExpressHci.c
@@ -2,7 +2,7 @@
   NvmExpressDxe driver is used to manage non-volatile memory subsystem which follows
   NVM Express specification.
 
-  Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2013 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -80,9 +80,6 @@ NvmHcRwMmio (
       MmioWrite64 (Address + Offset, * (UINT64 *)Data);
     }
     break;
-  default:
-    ASSERT (FALSE);
-    return EFI_INVALID_PARAMETER;
   }
 
   return EFI_SUCCESS;

BootloaderCommonPkg/Library/SecureBootLib/SecureBootHash.c
@@ -1,7 +1,7 @@
 /** @file
   Secure boot library routines to provide hash verification.
 
-  Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -179,8 +179,6 @@ DoHashVerify (
     DigestSize = SHA256_DIGEST_SIZE;
   } else if (HashAlg == HASH_TYPE_SHA384) {
     DigestSize = SHA384_DIGEST_SIZE;
-  } else {
-    return RETURN_INVALID_PARAMETER;
   }
 
   Status = CalculateHash (Data, Length, HashAlg, Digest);

BootloaderCommonPkg/Library/ShellLib/CmdPerf.c
@@ -1,7 +1,7 @@
 /** @file
   Shell command `perf` to display system performance data.
 
-  Copyright (c) 2017 - 2022, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2017 - 2023, Intel Corporation. All rights reserved.<BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -94,9 +94,6 @@ PrintCsmePerformanceInfo (
   }
 
   CsmePerformanceInfo = (CSME_PERFORMANCE_INFO *)GET_GUID_HOB_DATA (GuidHob);
-  if (CsmePerformanceInfo == NULL) {
-    return;
-  }
 
   ShellPrint (L"CSME Performance Info\n");
   ShellPrint (L"=======================\n\n");

BootloaderCommonPkg/Library/UfsBlockIoLib/UfsBlockIoLib.c
@@ -1499,62 +1499,55 @@ InitializeUfs (
       continue;
     }
 
-    if((RefClkFreq == UfsCardRefClkFreq19p2Mhz) ||
-       (RefClkFreq == UfsCardRefClkFreq26Mhz) ||
-       (RefClkFreq == UfsCardRefClkFreq38p4Mhz))
-    {
-      DEBUG ((DEBUG_INFO, "Setting clock frequency for UFS\n"));
-      RefClkAttr = UfsAttrRefClkFreq;
-      Attributes = UfsCardRefClkFreqObsolete;
-      Status     = UfsRwAttributes (Private, TRUE, RefClkAttr, 0, 0, (UINT32 *)&Attributes);
-      if (!EFI_ERROR (Status)) {
-        if (Attributes != RefClkFreq) {
-          Attributes = RefClkFreq;
+    DEBUG ((DEBUG_INFO, "Setting clock frequency for UFS\n"));
+    RefClkAttr = UfsAttrRefClkFreq;
+    Attributes = UfsCardRefClkFreqObsolete;
+    Status     = UfsRwAttributes (Private, TRUE, RefClkAttr, 0, 0, (UINT32 *)&Attributes);
+    if (!EFI_ERROR (Status)) {
+      if (Attributes != RefClkFreq) {
+        Attributes = RefClkFreq;
+        DEBUG (
+          (DEBUG_INFO,
+            "Setting bRefClkFreq attribute(%x) to %x\n  0 -> 19.2 Mhz\n  1 -> 26 Mhz\n  2 -> 38.4 Mhz\n  3 -> Obsolete\n",
+            RefClkAttr,
+            Attributes)
+          );
+        Status = UfsRwAttributes (Private, FALSE, RefClkAttr, 0, 0, (UINT32 *)&Attributes);
+        if (EFI_ERROR (Status)) {
           DEBUG (
-            (DEBUG_INFO,
-             "Setting bRefClkFreq attribute(%x) to %x\n  0 -> 19.2 Mhz\n  1 -> 26 Mhz\n  2 -> 38.4 Mhz\n  3 -> Obsolete\n",
-             RefClkAttr,
-             Attributes)
+            (DEBUG_ERROR,
+              "Failed to Change Reference Clock Attribute to %d, Status = %r \n",
+              RefClkFreq,
+              Status)
             );
-          Status = UfsRwAttributes (Private, FALSE, RefClkAttr, 0, 0, (UINT32 *)&Attributes);
-          if (EFI_ERROR (Status)) {
-            DEBUG (
-              (DEBUG_ERROR,
-               "Failed to Change Reference Clock Attribute to %d, Status = %r \n",
-               RefClkFreq,
-               Status)
-              );
-          }
         }
-      } else {
-        DEBUG (
-          (DEBUG_ERROR,
-           "Failed to Read Reference Clock Attribute, Status = %r \n",
-           Status)
-          );
-      }
-      if (Attributes == 0){
-        DEBUG (
-              (DEBUG_INFO,
-               "Setting bRefClkFreq attribute(%x) to 19.2 Mhz\n",
-               RefClkAttr)
-            );
-      } else if (Attributes == 1) {
-          DEBUG (
-              (DEBUG_INFO,
-               "Setting bRefClkFreq attribute(%x) to 26 Mhz\n",
-               RefClkAttr)
-          );
-      } else if (Attributes == 2) {
-          DEBUG (
-              (DEBUG_INFO,
-               "Setting bRefClkFreq attribute(%x) to 38.4 Mhz\n",
-               RefClkAttr)
-          );
       }
     } else {
-        DEBUG ((DEBUG_WARN, "Warning: Unsuppored UFS clock frequency \n"));
-      }
+      DEBUG (
+        (DEBUG_ERROR,
+          "Failed to Read Reference Clock Attribute, Status = %r \n",
+          Status)
+        );
+    }
+    if (Attributes == 0){
+      DEBUG (
+            (DEBUG_INFO,
+              "Setting bRefClkFreq attribute(%x) to 19.2 Mhz\n",
+              RefClkAttr)
+          );
+    } else if (Attributes == 1) {
+        DEBUG (
+            (DEBUG_INFO,
+              "Setting bRefClkFreq attribute(%x) to 26 Mhz\n",
+              RefClkAttr)
+        );
+    } else if (Attributes == 2) {
+        DEBUG (
+            (DEBUG_INFO,
+              "Setting bRefClkFreq attribute(%x) to 38.4 Mhz\n",
+              RefClkAttr)
+        );
+    }
 
     Status = VerifyUfsHcPlatformPostHceSwitchGear(Private);
     if (EFI_ERROR (Status)) {

commit 70f88e1345e46feced918981e05f0eefefb7cc56

feat: Fix device enumerate in NVMe library
There is a NVMe regression caused by https://github.com/slimbootloader/slimbootloader/pull/1958.
The NVMe device enumerated should not be freed since it will be used in other APIs.

Signed-off-by: Guo Dong <guo.dong@intel.com>

BootloaderCommonPkg/Library/NvmExpressLib/NvmExpress.c
@@ -177,9 +177,6 @@ Exit:
     FreePool (NamespaceData);
   }
 
-  if (Device != NULL) {
-    FreePool (Device);
-  }
   return Status;
 }
 