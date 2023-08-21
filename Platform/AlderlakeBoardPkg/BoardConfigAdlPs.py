e4ff07171f30315ec638cf183e6817d023dd65b5
Vincent Chen <vincent.chen@intel.com>
2023-08-22 05:46:02 +0800

    [ADLPS] Update FSP/UCODE/VBT for MR3 release (#1999)
    - update FSP version to IoT ADL-PS MR3 (0C.01.A4.14)
    - update microcode version to 42C
    - update VBT version to 251
    - update platform version to 1.3
    
    Signed-off-by: Vincent Chen <vincent.chen@intel.com>

--- Platform/AlderlakeBoardPkg/BoardConfigAdlPs.py
+++ Platform/AlderlakeBoardPkg/BoardConfigAdlPs.py
@@ -1,7 +1,7 @@
 ## @file
 # This file is used to provide board specific image information.
 #
-#  Copyright (c) 2022, Intel Corporation. All rights reserved.<BR>
+#  Copyright (c) 2022 - 2023, Intel Corporation. All rights reserved.<BR>
 #
 #  SPDX-License-Identifier: BSD-2-Clause-Patent
 #
@@ -23,7 +23,7 @@ class Board(AlderlakeBoardConfig.Board):
         self.VERINFO_IMAGE_ID     = 'SB_ADLPS'
         self.BOARD_NAME           = 'adlps'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 1
+        self.VERINFO_PROJ_MINOR_VER = 3
         self._EXTRA_INC_PATH      = ['Silicon/AlderlakePkg/Adlps/Include']
         self._FSP_PATH_NAME       = 'Silicon/AlderlakePkg/Adlps/FspBin'
         self.MICROCODE_INF_FILE   = 'Silicon/AlderlakePkg/Microcode/Microcode.inf'
