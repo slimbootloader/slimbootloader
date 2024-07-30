34a701932dfa00a20c8516e8c9caaba4d955f226
Randy <randy.lin@intel.com>
2024-07-30 15:22:55 +0800

    feat: [RPLS] Update MR4 Release
    FSP version is 0C00DE40
    platform version is 1.4
    Microcode Files are: ['m_07_90672_00000036.pdb', 'm_32_b0671_00000123.pdb']
    
    Signed-off-by: Randy <randy.lin@intel.com>

--- Platform/RaptorlakeBoardPkg/BoardConfigRpls.py
+++ Platform/RaptorlakeBoardPkg/BoardConfigRpls.py
@@ -24,7 +24,7 @@ class Board(BaseBoard):
 
         self.VERINFO_IMAGE_ID     = 'SB_RPLS'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 3
+        self.VERINFO_PROJ_MINOR_VER = 4
         self.VERINFO_SVN            = 1
         self.VERINFO_BUILD_DATE     = time.strftime("%m/%d/%Y")
 
