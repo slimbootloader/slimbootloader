f6b83525b0144f0172ce48424bb7419fe63720ed
Bejean Mosher <bejean.mosher@intel.com>
2024-09-03 20:53:47 -0400

    fix: Source Level Debug breaks in X64.
    DebugAgentLib was using its own IDT Entry structure definition that
    didn't adjust for x64. Removed this definition and switched to built
    in definition from ProcessorBind.h which does adjust for architecture
    correctly.
    
    Signed-off-by: Bejean Mosher <bejean.mosher@intel.com>

--- BootloaderCommonPkg/Library/DebugAgentLib/DebugAgent.h
+++ BootloaderCommonPkg/Library/DebugAgentLib/DebugAgent.h
@@ -131,21 +131,6 @@ typedef struct {
 } DEBUG_AGENT_MAILBOX;
 #pragma pack()
 
-///
-/// Byte packed structure for an IA-32 Interrupt Gate Descriptor.
-///
-typedef union {
-  struct {
-    UINT32  OffsetLow:16;   ///< Offset bits 15..0.
-    UINT32  Selector:16;    ///< Selector.
-    UINT32  Reserved_0:8;   ///< Reserved.
-    UINT32  GateType:8;     ///< Gate Type.  See #defines above.
-    UINT32  OffsetHigh:16;  ///< Offset bits 31..16.
-  } Bits;
-  UINT64  Uint64;
-} IA32_IDT_ENTRY;
-
-
 typedef union {
   struct {
     UINT32  LimitLow    : 16;
