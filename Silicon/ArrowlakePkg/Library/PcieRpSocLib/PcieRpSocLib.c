86079cea9e703c7e85662360752bfcfc0096c564
Guo Dong <guo.dong@intel.com>
2024-09-25 22:24:16 -0700

    ARL: Update PcieRbLib
    Update MtlPchPcieRpLib and PcieRpSocLib.
    Late could combine these 2 libraries.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

--- Silicon/ArrowlakePkg/Library/PcieRpSocLib/PcieRpSocLib.c
+++ Silicon/ArrowlakePkg/Library/PcieRpSocLib/PcieRpSocLib.c
@@ -12,7 +12,7 @@
 #include <Library/DebugLib.h>
 #include <Library/BaseLib.h>
 #include <Library/PciSegmentLib.h>
-#include <Include/P2SbSidebandAccessLib.h>
+#include <Library/P2SbLib.h>
 #include <Include/PcieRegs.h>
 #include <Register/PchRegs.h>
 #include <Register/PchBdfAssignment.h>
@@ -22,32 +22,37 @@
 #include <Library/P2SbSocLib.h>
 #include <Library/PchInfoLib.h>
 #include <Library/BaseMemoryLib.h>
-#include <Include/P2SbSidebandAccessLib.h>
+#include <Library/BootloaderCommonLib.h>
+#include <Library/PciLib.h>
+#include <Guid/OsBootOptionGuid.h>
+
+#define DEVICE_TABLE_P2SB_INSTANCE_SOC     0
+#define DEVICE_TABLE_P2SB_INSTANCE_IOE     1
+#define DEVICE_TABLE_P2SB_INSTANCE_PCH     2
 
 typedef struct {
   UINT8     DevNum;
   P2SB_PID  Pid;
-  UINTN     RpNumBase;
+  UINT8     RpNumBase;
   UINT8     NumOfRp;
   BOOLEAN   OnIoe;
-  BOOLEAN   IocDecoded;
 } MTL_SOC_PCIE_CONTROLLER_INFO;
 
 GLOBAL_REMOVE_IF_UNREFERENCED MTL_SOC_PCIE_CONTROLLER_INFO  mMtlSocMPcieControllerInfo[] = {
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_1, MTL_SOC_PID_SPA,  0, 4, FALSE, FALSE },
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_5, MTL_SOC_PID_SPB,  4, 4, FALSE, FALSE },
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_9, MTL_SOC_PID_SPC,  8, 1, FALSE, TRUE },
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPD, 9, 1, TRUE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_11, MTL_SOC_PID_PXPE, 10, 1, TRUE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_12, MTL_SOC_PID_PXPF, 11, 1, TRUE, TRUE}
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_1,  MTL_SOC_PID_SPA,  0,  4, FALSE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_5,  MTL_SOC_PID_SPB,  4,  4, FALSE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_9,  MTL_SOC_PID_SPC,  8,  1, FALSE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPD, 9,  1, TRUE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_11, MTL_SOC_PID_PXPE, 10, 1, TRUE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_12, MTL_SOC_PID_PXPF, 11, 1, TRUE }
 };
 
 GLOBAL_REMOVE_IF_UNREFERENCED MTL_SOC_PCIE_CONTROLLER_INFO  mMtlSocSPcieControllerInfo[] = {
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPA, 0, 2, FALSE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPB, 2, 1, FALSE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPD, 3, 1, TRUE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_12, MTL_SOC_PID_PXPF, 4, 1, TRUE, TRUE},
-  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_11, MTL_SOC_PID_PXPE, 5, 1, TRUE, TRUE}
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPA, 0, 2, FALSE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPB, 2, 1, FALSE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_10, MTL_SOC_PID_PXPD, 3, 1, TRUE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_12, MTL_SOC_PID_PXPF, 4, 1, TRUE },
+  { PCI_DEVICE_NUMBER_PCH_PCIE_ROOT_PORT_11, MTL_SOC_PID_PXPE, 5, 1, TRUE }
 };
 
 /**
@@ -63,27 +68,28 @@ MtlSocRpIndexToControllerIndex (
   IN UINTN  RpIndex
   )
 {
-  if (MtlIsSocM ()) {
-    if (RpIndex < 4) {
-        return 0;
-    } else if (RpIndex < 8) {
-        return 1;
-    } else if (RpIndex == 8) {
-        return 2;
-    } else if (RpIndex == 9) {
-        return 3;
-    } else if (RpIndex == 10) {
-        return 4;
-    } else if (RpIndex == 11) {
-        return 5;
-    }
-  } else if (MtlIsSocS ()) {
-    if (RpIndex < 2) {
-      return 0;
-    } else if (RpIndex <= 6) {
-      return (UINT32) RpIndex - 1;
-    }
+#if FixedPcdGetBool(PcdMtlSSupport)
+  if (RpIndex < 2) {
+    return 0;
+  } else if (RpIndex <= 6) {
+    return (UINT32) RpIndex - 1;
   }
+#else
+  if (RpIndex < 4) {
+    return 0;
+  } else if (RpIndex < 8) {
+    return 1;
+  } else if (RpIndex == 8) {
+    return 2;
+  } else if (RpIndex == 9) {
+    return 3;
+  } else if (RpIndex == 10) {
+    return 4;
+  } else if (RpIndex == 11) {
+    return 5;
+  }
+#endif
+
   return 0xFF;
 }
 
@@ -99,33 +105,75 @@ MtlSocPcieRpGetSbiFid (
   IN UINTN  RpIndex
   )
 {
-  if (MtlIsSocM ()) {
-    if (RpIndex < 8) {
-      return (UINT16)(RpIndex % 4);
-    } else {
-      return 0;
-    }
+#if FixedPcdGetBool(PcdMtlSSupport)
+  if (RpIndex < 2) {
+    return (UINT16) RpIndex;
   } else {
-    if (RpIndex < 2) {
-      return (UINT16) RpIndex;
-    } else {
-      return 0;
+    return 0;
+  }
+#else
+  if (RpIndex < 8) {
+    return (UINT16)(RpIndex % 4);
+  } else {
+    return 0;
+  }
+#endif
+}
+
+
+/**
+  Get device address
+
+  If the device is PCI device, the device address format is 0x00BBDDFF, where
+  BB, DD and FF are PCI bus, device and function number.
+  If the device is MMIO device, the device address format is 0x0000MMMMMM000000, where
+  MM should be non-zero value.
+
+  @param[in]  DeviceInstance     The device instance number starting from 0.
+
+  @retval     Device address for a given device instance, return 0 if the device
+              could not be found from device table.
+**/
+UINT64
+GetP2sbDeviceAddr2 (
+  IN  UINT8          DeviceInstance
+  )
+{
+  PLT_DEVICE_TABLE   *DeviceTable;
+  PLT_DEVICE         *Device;
+  UINT64             DeviceBase;
+  UINT32             Index;
+
+  DeviceBase  = 0;
+  Device      = NULL;
+  DeviceTable = (PLT_DEVICE_TABLE *)GetDeviceTable();
+  for (Index = 0; Index < DeviceTable->DeviceNumber; Index++) {
+    Device = &DeviceTable->Device[Index];
+    if ((Device->Type == PlatformDeviceP2sb) && (Device->Instance == DeviceInstance)){
+      break;
     }
   }
+
+  if (DeviceTable->DeviceNumber != Index) {
+    DeviceBase = Device->Dev.DevAddr | ((UINT64)Device->Reserved << 32);
+  }
+
+  return DeviceBase;
 }
 
 
 /**
   Get MTL SoC Pcie Root Port Device and Function Number by Root Port physical Number
 
-  @param[in]  RpNumber              Root port physical number. (0-based)
-  @param[out] RpDev                 Return corresponding root port device number.
-  @param[out] RpFun                 Return corresponding root port function number.
+  @param[in]  RpNumber           Root port physical number. (0-based)
+  @param[out] RpDev              Return corresponding root port device number.
+  @param[out] RpFun              Return corresponding root port function number.
 
   @retval     EFI_SUCCESS           Root port device and function is retrieved
   @retval     EFI_INVALID_PARAMETER RpNumber is invalid
 **/
 EFI_STATUS
+EFIAPI
 MtlSocGetPcieRpDevFun (
   IN  UINTN    RpNumber,
   OUT UINT16   *RpDev,
@@ -136,148 +184,41 @@ MtlSocGetPcieRpDevFun (
   UINTN                          FuncIndex;
   UINT32                         PciePcd;
   MTL_SOC_PCIE_CONTROLLER_INFO   *PcieInfo;
-  P2SB_SIDEBAND_REGISTER_ACCESS  RpAccess;
-  P2SB_SIDEBAND_ACCESS_METHOD    AccessMethod;
-  P2SB_CONTROLLER                P2SbController;
+  UINT64                          P2sbBase;
+  UINT16                          Fid;
 
-  PcieInfo = NULL;
-  if (MtlIsSocM ()) {
-    PcieInfo = mMtlSocMPcieControllerInfo;
-  } else if (MtlIsSocS ()) {
-    PcieInfo = mMtlSocSPcieControllerInfo;
-  }
-  Index    = MtlSocRpIndexToControllerIndex (RpNumber);
-  *RpDev   = 0xFF;
-  *RpFun   = 0xFF;
-
-  if (Index == 0xFF || PcieInfo == NULL) {
-    return EFI_NOT_FOUND;
+  Index = MtlSocRpIndexToControllerIndex (RpNumber);
+  if (Index == 0xFF) {
+    return EFI_INVALID_PARAMETER;
   }
 
-  ZeroMem (&RpAccess, sizeof (P2SB_SIDEBAND_REGISTER_ACCESS));
-  FuncIndex = RpNumber - PcieInfo[Index].RpNumBase;
-  *RpDev = PcieInfo[Index].DevNum;
+#if FixedPcdGetBool(PcdMtlSSupport)
+  PcieInfo = mMtlSocSPcieControllerInfo;
+#else
+  PcieInfo = mMtlSocMPcieControllerInfo;
+#endif
+
   if (PcieInfo[Index].OnIoe) {
-    MtlIoeGetP2SbController (&P2SbController);
-    if (MtlIoeIsP2SbHidden ()) {
-      AccessMethod = P2SbMmioAccess;
-    } else {
-      AccessMethod = P2SbMsgAccess;
-    }
-    BuildP2SbSidebandAccess (
-      &P2SbController,
-      PcieInfo[Index].Pid,
-      MtlSocPcieRpGetSbiFid (RpNumber),
-      P2SbPrivateConfig,
-      AccessMethod,
-      FALSE,
-      &RpAccess
-      );
+    P2sbBase = GetP2sbDeviceAddr2 (DEVICE_TABLE_P2SB_INSTANCE_IOE);
   } else {
-    MtlSocGetP2SbController (&P2SbController);
-    BuildP2SbSidebandAccess (
-      &P2SbController,
-      PcieInfo[Index].Pid,
-      MtlSocPcieRpGetSbiFid (RpNumber),
-      P2SbPrivateConfig,
-      P2SbMmioAccess,
-      FALSE,
-      &RpAccess
-      );
+    P2sbBase = GetP2sbDeviceAddr2 (DEVICE_TABLE_P2SB_INSTANCE_SOC);
   }
-  PciePcd = RpAccess.Access.Read32 (&RpAccess.Access, R_SPX_SIP16_PCR_PCD);
+  DEBUG ((DEBUG_INFO, "P2sbBase 0x%llx\n",P2sbBase));
+
+  if ((P2sbBase & 0x0FFFFFFF) != 0) {
+    P2sbBase = TO_PCI_LIB_ADDRESS (P2sbBase);
+  }
+
+  *RpDev    = PcieInfo[Index].DevNum;
+  *RpFun    = 0xFF;
+  FuncIndex = RpNumber - PcieInfo[Index].RpNumBase;
+  Fid       = MtlSocPcieRpGetSbiFid (RpNumber);
+  PciePcd   = P2sbRead32 (P2sbBase, PcieInfo[Index].Pid, Fid, R_SPX_SIP16_PCR_PCD);
   if (PciePcd != 0xFFFFFFFF) {
     *RpFun = (PciePcd >> (FuncIndex * S_SPX_PCR_PCD_RP_FIELD)) & B_SPX_PCR_PCD_RP1FN;
   }
+  DEBUG ((DEBUG_INFO, "%d, P2sbBase 0x%llx, PID=0x%x, Fid = 0x%x, PciePcd = 0x%x\n", Index, P2sbBase, PcieInfo[Index].Pid, Fid, PciePcd));
+
   return EFI_SUCCESS;
 }
 
-/**
-  This function returns PID according to PCIe controller index
-
-  @param[in] ControllerIndex     PCIe controller index
-
-  @retval P2SB_PID    Returns PID for SBI Access
-**/
-P2SB_PID
-MtlSocGetPcieControllerSbiPid (
-  IN UINT32  ControllerIndex
-  )
-{
-  if (MtlIsSocM()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocMPcieControllerInfo)) {
-      return mMtlSocMPcieControllerInfo[ControllerIndex].Pid;
-    }
-  } else if (MtlIsSocS()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocSPcieControllerInfo)) {
-      return mMtlSocSPcieControllerInfo[ControllerIndex].Pid;
-    }
-  }
-  return 0xFF;
-}
-
-/**
-  This function returns PID according to Root Port Number
-
-  @param[in] RpIndex     Root Port Index (0-based)
-
-  @retval P2SB_PID   Returns PID for SBI Access
-**/
-P2SB_PID
-MtlSocGetRpSbiPid (
-  IN UINTN  RpIndex
-  )
-{
-  UINT32   ControllerIndex;
-
-  ControllerIndex = MtlSocRpIndexToControllerIndex ((UINTN)RpIndex);
-  return MtlSocGetPcieControllerSbiPid (ControllerIndex);
-}
-
-/**
-  Checks whether RpIndex is located on IOE die.
-
-  @param[in] RpIndex  Root port index.
-
-  @return TRUE if root port is located on IOE die.
-**/
-BOOLEAN
-MtlSocIsRpOnIoeDie (
-  IN UINT32  RpIndex
-  )
-{
-  UINT32  ControllerIndex;
-
-  ControllerIndex = MtlSocRpIndexToControllerIndex ((UINTN)RpIndex);
-  if (MtlIsSocM()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocMPcieControllerInfo)) {
-      return mMtlSocMPcieControllerInfo[ControllerIndex].OnIoe;
-    }
-  } else if (MtlIsSocS()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocSPcieControllerInfo)) {
-      return mMtlSocSPcieControllerInfo[ControllerIndex].OnIoe;
-    }
-  }
-  return FALSE;
-}
-
-BOOLEAN
-MtlSocIsRpIocDecoded (
-  IN UINT32  RpIndex
-  )
-{
-  UINT32  ControllerIndex;
-
-  ControllerIndex = MtlSocRpIndexToControllerIndex ((UINTN)RpIndex);
-  if (MtlIsSocM()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocMPcieControllerInfo)) {
-      return mMtlSocMPcieControllerInfo[ControllerIndex].IocDecoded;
-    }
-  } else if (MtlIsSocS()) {
-    if (ControllerIndex < ARRAY_SIZE (mMtlSocSPcieControllerInfo)) {
-      return mMtlSocSPcieControllerInfo[ControllerIndex].IocDecoded;
-    }
-  }
-
-  return FALSE;
-}
