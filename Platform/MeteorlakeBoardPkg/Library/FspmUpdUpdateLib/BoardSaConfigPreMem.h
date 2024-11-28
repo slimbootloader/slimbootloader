cb12cdd6bf0163c00049b82c7c0171257142318f
Randy <randy.lin@intel.com>
2024-11-28 14:43:00 +0800

    fix: [MCL] HDMI port no output
    Update VBT & HPD/DDC setting
    
    Signed-off-by: Randy <randy.lin@intel.com>

--- Platform/MeteorlakeBoardPkg/Library/FspmUpdUpdateLib/BoardSaConfigPreMem.h
+++ Platform/MeteorlakeBoardPkg/Library/FspmUpdUpdateLib/BoardSaConfigPreMem.h
@@ -146,20 +146,20 @@ GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 mMtlPDdr5SODimmSbsCrbRowDisplayDdiConf
 GLOBAL_REMOVE_IF_UNREFERENCED const UINT8 mMclRowDisplayDdiConfig[16] = {
                                                                               DdiPortDisabled,    // DDI Port A Config : DdiPortDisabled = No LFP is Connected, DdiPortEdp = eDP, DdiPortMipiDsi = MIPI DSI
                                                                               DdiPortDisabled,    // DDI Port B Config : DdiPortDisabled = No LFP is Connected, DdiPortEdp = eDP, DdiPortMipiDsi = MIPI DSI
-                                                                              DdiHpdDisable,       // DDI Port A HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                              DdiHpdDisable,       // DDI Port B HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdDisable,      // DDI Port A HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdEnable,       // DDI Port B HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                               DdiHpdDisable,      // DDI Port C HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                              DdiHpdDisable,       // DDI Port 1 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                              DdiHpdDisable,       // DDI Port 2 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                              DdiHpdDisable,      // DDI Port 3 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
-                                                                              DdiHpdDisable,      // DDI Port 4 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdDisable,      // DDI Port 1 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdDisable,      // DDI Port 2 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdEnable,       // DDI Port 3 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
+                                                                              DdiHpdEnable,       // DDI Port 4 HPD    : DdiHpdDisable = Disable, DdiHpdEnable = Enable HPD
                                                                               DdiDisable,         // DDI Port A DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
-                                                                              DdiDisable,       // DDI Port B DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                              DdiDdcEnable,       // DDI Port B DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
                                                                               DdiDisable,         // DDI Port C DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
-                                                                              DdiDisable,       // DDI Port 1 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
-                                                                              DdiDisable,       // DDI Port 2 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
-                                                                              DdiDisable,         // DDI Port 3 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
-                                                                              DdiDisable          // DDI Port 4 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                              DdiDisable,         // DDI Port 1 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                              DdiDisable,         // DDI Port 2 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                              DdiDdcEnable,       // DDI Port 3 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
+                                                                              DdiDdcEnable        // DDI Port 4 DDC    : DdiDisable = Disable, DdiDdcEnable = Enable DDC
 };
 
 #endif // _BOARD_SA_CONFIG_PRE_MEM_H_
