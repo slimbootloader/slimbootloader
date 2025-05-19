3d3c6375866491c0cc39c90c4ef295fa7f2e3131
Kevin Tsai <kevin.tsai@intel.com>
2025-05-19 09:31:40 -0700

    fix: [ARL] Set SMI lock bit for Osloader
    Set SMI lock bit at ReadyToBoot for Osloader
    Uefi payload will set this bit in its driver.
    
    Signed-off-by: Kevin Tsai <kevin.tsai@intel.com>

--- Platform/ArrowlakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
+++ Platform/ArrowlakeBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
@@ -141,6 +141,10 @@ ProgramSecuritySetting (
 
   // Set the BIOS Lock Enable and EISS bits
   MmioOr8 (SpiBaseAddress + R_SPI_CFG_BC, (UINT8) (B_SPI_CFG_BC_LE | B_SPI_CFG_BC_EISS));
+
+  // Set SMI LOCK (SMI_LOCK)
+  DEBUG ((DEBUG_INFO, "Set SMI Lock\n"));
+  MmioOr8 (PCH_PWRM_BASE_ADDRESS + R_PMC_PWRM_GEN_PMCON_B, (UINT8)B_PMC_PWRM_GEN_PMCON_B_SMI_LOCK);
 }
 
 /**
