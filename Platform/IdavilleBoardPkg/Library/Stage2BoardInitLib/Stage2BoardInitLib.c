d81d535c5969d450ded84986e8e47a5fba2ddbf4
Perni <ramesh.chandra.perni@intel.com>
2025-05-26 17:30:55 +0530

    ICXD:Set SMI lock bit at ReadyToBoot for Osloader.
     Test info: Confirmed SMI_LOCK setting in OSLoader shell
    
    Signed-off-by: Perni <ramesh.chandra.perni@intel.com>

--- Platform/IdavilleBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
+++ Platform/IdavilleBoardPkg/Library/Stage2BoardInitLib/Stage2BoardInitLib.c
@@ -192,6 +192,12 @@ BoardInit (
   case ReadyToBoot:
     if ((GetBootMode() != BOOT_ON_FLASH_UPDATE) && (GetPayloadId() == 0)) {
       ProgramSecuritySetting ();
+      //
+      // set SMI LOCK (SMI_LOCK)
+      // PWRM Offset 1024h Bit 4
+      //
+      DEBUG ((DEBUG_INFO, "Set SMI Lock\n"));
+      MmioOr8 (PCH_PWRM_BASE_ADDRESS + R_PMC_PWRM_GEN_PMCON_B, (UINT8)B_PMC_PWRM_GEN_PMCON_B_SMI_LOCK);
     }
     break;
   case EndOfFirmware:
