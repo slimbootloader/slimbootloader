267abb76420ef4295c48586d5973a2601868ff99
ramesh chandra perni <ramesh.chandra.perni@intel.com>
2025-06-19 17:39:13 +0530

    feat: [AZB] Updated Hash for  Microcode/FSP and SBL Version AZB IPU 25.4
    Updated FSP and Microcode Hash. Update the AZB SBL version to 1.6 for
    IPU 25.4
    
    Signed-off-by: ramesh chandra perni <ramesh.chandra.perni@intel.com>

--- Platform/AlderlakeBoardPkg/BoardConfigAzb.py
+++ Platform/AlderlakeBoardPkg/BoardConfigAzb.py
@@ -24,7 +24,7 @@ class Board(BaseBoard):
         super(Board, self).__init__(*args, **kwargs)
         self.VERINFO_IMAGE_ID                 = 'SB_AZB'
         self.VERINFO_PROJ_MAJOR_VER           = 1
-        self.VERINFO_PROJ_MINOR_VER           = 5
+        self.VERINFO_PROJ_MINOR_VER           = 6
         self.VERINFO_CORE_MAJOR_VER           = 1
         self.VERINFO_CORE_MINOR_VER           = 0
         self.VERINFO_SVN                      = 1
