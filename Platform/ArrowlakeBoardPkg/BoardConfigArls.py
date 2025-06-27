52edc3c42a237a13b9417c22089762361a688e61
Randy <randy.lin@intel.com>
2025-06-27 14:08:00 +0800

    feat: [ARL] MR1 release
     ARLUH FSP:0D00E610
     ARLS  FSP:0D00E520
     Microcode:ArrowLake/m_80_b0650_0000000a.pdb
     Microcode:ArrowLake/m_82_c0662_00000118.pdb
     VBT update to 1062.
     Update SBL version to 1.1
    
    Signed-off-by: Randy <randy.lin@intel.com>

--- Platform/ArrowlakeBoardPkg/BoardConfigArls.py
+++ Platform/ArrowlakeBoardPkg/BoardConfigArls.py
@@ -23,8 +23,8 @@ class Board(BaseBoard):
         super(Board, self).__init__(*args, **kwargs)
 
         self.VERINFO_IMAGE_ID     = 'SB_ARLS'
-        self.VERINFO_PROJ_MAJOR_VER = 0
-        self.VERINFO_PROJ_MINOR_VER = 0
+        self.VERINFO_PROJ_MAJOR_VER = 1
+        self.VERINFO_PROJ_MINOR_VER = 1
         self.VERINFO_SVN            = 1
         self.VERINFO_BUILD_DATE     = time.strftime("%m/%d/%Y")
 
