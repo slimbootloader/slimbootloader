637ac4bd4a87a5feff71c1a7bf17af3e3a2e5119
Vincent Chen <vincent.chen@intel.com>
2025-09-05 11:07:40 +0800

    feat: [ASL/TWL] Update for ASL MR4, TWL MR1
    - FSP: IoT TWL MR1/ASL MR4 (0C.02.89.40)
    - Microcode: m_19_b06e0_0000001d
    - VBT: 256
    - platform version: SB_ASL-1.4
    
    Signed-off-by: Vincent Chen <vincent.chen@intel.com>

--- Platform/AlderlakeBoardPkg/BoardConfigAdlN.py
+++ Platform/AlderlakeBoardPkg/BoardConfigAdlN.py
@@ -22,9 +22,9 @@ class Board(BaseBoard):
 
         super(Board, self).__init__(*args, **kwargs)
 
-        self.VERINFO_IMAGE_ID     = 'SB_ADLN'
+        self.VERINFO_IMAGE_ID     = 'SB_ASL'
         self.VERINFO_PROJ_MAJOR_VER = 1
-        self.VERINFO_PROJ_MINOR_VER = 7
+        self.VERINFO_PROJ_MINOR_VER = 4
         self.VERINFO_SVN            = 1
         self.VERINFO_BUILD_DATE     = time.strftime("%m/%d/%Y")
 
