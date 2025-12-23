40b926c6a29fb2b70fb30e549fb3b62d29cfc18b
Bejean Mosher <bejean.mosher@intel.com>
2025-12-23 13:57:20 -0500

    feat: Enable FSP-S PE/TE Rebase based on board configuration
    FSP Spec says FSP-S must be rebased when executed from address other than
    its preferred address. However, SBL was not doing this since it is not
    required in most cases. Recent changes to FSP-S SEC in latest EDK2
    IntelFsp2Pkg cause this to be necessary when FSP is built with GCC in
    some cases.
    
    Signed-off-by: Bejean Mosher <bejean.mosher@intel.com>

--- BootloaderCorePkg/Library/FspApiLib/FspMisc.c
+++ BootloaderCorePkg/Library/FspApiLib/FspMisc.c
@@ -36,31 +36,3 @@ FspResetHandler (
   }
 }
 
-/**
-  This FSP API is used to notify the FSP about the different phases in the boot process.
-  This allows the FSP to take appropriate actions as needed during different initialization
-  phases. The phases will be platform dependent and will be documented with the FSP
-  release. The current FSP supports following notify phases:
-    Post PCI enumeration
-    Ready To Boot
-    End of firmware
-
-  @param[in] Delta              The delta between the new and old base.
-                                Delta = NewBase - OldBase
-
-  @retval EFI_SUCCESS           The rebasing was handled successfully.
-
-**/
-EFI_STATUS
-EFIAPI
-RebaseFspComponent (
-  UINT32   Delta
-  )
-{
-  FSP_INFO_HEADER   *FspHeader;
-
-  FspHeader = (FSP_INFO_HEADER *)(UINTN)(PcdGet32 (PcdFSPSBase) + Delta + FSP_INFO_HEADER_OFF);
-  FspHeader->ImageBase += Delta;
-
-  return EFI_SUCCESS;
-}
