d6d2920ec58db9930eb1f2aef501302616a89755
Kolhe Chirag Vijay <chirag.vijay.kolhe@intel.com>
2026-06-29 20:15:41 +0530

    feat: [ARLS/U/H] Use 64-bit PcdLib API to get PCI CFG space base
    Use 64-bit PcdLib API to get PCI CFG space base address for Arrowlake
    platforms. The UPD to which the PcdPciMmcfgBase (PCI CFG space base
    address) gets assigned is already 64-bit.
    
    Signed-off-by: Kolhe Chirag Vijay <chirag.vijay.kolhe@intel.com>

--- Platform/ArrowlakeBoardPkg/Library/Stage1ABoardInitLib/Stage1ABoardInitLib.c
+++ Platform/ArrowlakeBoardPkg/Library/Stage1ABoardInitLib/Stage1ABoardInitLib.c
@@ -61,7 +61,7 @@ FSPT_UPD TempRamInitParams = {
     .PcdSerialIoUartNumber      = FixedPcdGet32 (PcdDebugPortNumber),
     .PcdSerialIoUartMode        = 3,
     .PcdSerialIoUartBaudRate    = 115200,
-    .PcdPciExpressBaseAddress   = FixedPcdGet32 (PcdPciMmcfgBase),
+    .PcdPciExpressBaseAddress   = FixedPcdGet64 (PcdPciMmcfgBase),
     .PcdPciExpressRegionLength  = 0x10000000,
 
     .PcdLpcUartDebugEnable      = 1,
