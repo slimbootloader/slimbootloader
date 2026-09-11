b206796dc1f225cd4919b378335cb0358cf879a0
Kevin Tsai <kevin.tsai@intel.com>
2026-09-11 15:27:27 -0700

    [SblBuilderGui] Add FwUpdate tab for firmware capsule generation
    Add SblBuilder03_FwUpdatePage.py:
    - Run GenCapsuleFirmware.py CLI (-p/-k/-o/-a/-s/-f/-sa)
    - Payload Type fixed to BIOS (SBL/BIOS region updates only)
    - Payload file and signing key selectors with sensible defaults
      derived from the shared source/keys workspace paths
    - Editable output filename (defaults to FwuImage.bin)
    
    Rename SblBuilder03_AboutPage.py to SblBuilder04_AboutPage.py
    
    Signed-off-by: Kevin Tsai <kevin.tsai@intel.com>

--- BootloaderCorePkg/Tools/SblBuilderGui/tabs/SblBuilder04_AboutPage.py
+++ BootloaderCorePkg/Tools/SblBuilderGui/tabs/SblBuilder04_AboutPage.py
@@ -1,4 +1,4 @@
-## @ SblBuilder03_AboutPage.py
+## @ SblBuilder04_AboutPage.py
 #
 # Copyright (c) 2026, Intel Corporation. All rights reserved.<BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -22,7 +22,8 @@ class AboutPage(TabPage):
             "Main functions:\n"
             "- Setup workspace and source repository\n"
             "- Build selected board targets\n"
-            "- Manage payload-related options\n\n"
+            "- Manage payload-related options\n"
+            "- Generate signed firmware update capsules (FwUpdate)\n\n"
         )
 
         self.about_message = tk.Message(
